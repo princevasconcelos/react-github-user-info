ce8343318ba6d6c5a98441b40eab6ea9223c0b75
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-02-24 17:48:19 -0300

    Refactoring to a flexible sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './sidebar.css'
+
+const defaultProps = {
+  name: 'Prince Vasconcelos',
+  bio: 'Front End Developer',
+  avatar: 'https://avatars2.githubusercontent.com/u/27703956?v=4',
+}
+
+const propTypes = {
+  name: PropTypes.string,
+  bio: PropTypes.string,
+  avatar: PropTypes.string,
+}
 
 const Sidebar = ({name, bio, avatar}) => {
 
@@ -13,11 +26,7 @@ const Sidebar = ({name, bio, avatar}) => {
   return component;
 }
 
+Sidebar.propTypes = propTypes;
+Sidebar.defaultProps = defaultProps;
+
 export default Sidebar;
-
-//text-center
-
-      {/* <div className="wrapper">
-        <figcaption className='large-font'><strong>{name}</strong></figcaption>
-        <figcaption className='figure-caption'>{bio}</figcaption>
-      </div> */}
