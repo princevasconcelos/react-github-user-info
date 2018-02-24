55c3b50f80e876c77056aba1ef12938ecfcbbb8c
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-02-23 21:28:10 -0300

    Adding bootstrap 4

---
+++ src/components/Buttom.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Buttom extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type='button' className='btn btn-primary'>Click me</button>
+      </div>
+    );
+  }
+}
+
+export default Buttom;
