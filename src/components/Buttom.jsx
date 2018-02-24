c909d6ef8c96cbbc382d3d5170e7575b1bfb05bc
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-02-23 21:33:42 -0300

    Adding ionicons

--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Ionicon from 'react-ionicons'
 
 class Buttom extends React.Component {
   render() {
     return (
       <div>
-        <button type='button' className='btn btn-primary'>Click me</button>
+        <button type='button' className='btn btn-primary'>
+          <Ionicon icon="ios-add-circle" fontSize="35px" onClick={() => console.log('Hi!')} color="white"/>
+        </button>
       </div>
     );
   }
