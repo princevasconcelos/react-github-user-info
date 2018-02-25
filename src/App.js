7fee2e3a8e0964ea25e1ff712a44249c4ae35312
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-02-24 22:22:55 -0300

    Adding repository component to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar/navbar';
 import Sidebar from './components/sidebar/sidebar';
+//import Main from './components/main/main';
+import Repository from './components/repository/repository';
+
 
 class App extends Component {
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div>
@@ -11,10 +19,17 @@ class App extends Component {
           brandIcon='logo-github'
           title='Github'
           subtitle='profiles' />
-        <Sidebar 
+        {/* <Sidebar 
           name='Roger Ramos'
           bio='Front End Dev and UI Designer'
-          avatar='https://avatars0.githubusercontent.com/u/3275772?v=4' />
+          avatar='https://avatars0.githubusercontent.com/u/3275772?v=4' /> */}
+        <Repository 
+          name='todo-backend'
+          language='Javascript'
+          langIcon='md-code'
+          count='999'
+          countIcon='md-git-branch'
+          desc='descrição descrição descrição descrição descrição descrição ' />
       </div>
     );
   }
