0525b46c14e40800b6b1a7d79833f9637864120a
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-02-25 02:54:49 -0300

    Adding main componnet

---
+++ src/components/main/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ionicon from 'react-ionicons';
+import PropTypes from 'prop-types';
+import './main.css'
+
+const Main = ({}) => {
+  
+  const component = (
+    <div className='mainContent'>
+      <h1>oi</h1>
+    </div>
+  );
+  return component;
+}
+
+export default Main;
