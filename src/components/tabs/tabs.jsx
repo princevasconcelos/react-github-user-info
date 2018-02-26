032eeb433a9a80e402c3ad07bcc356fc9501478a
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-02-26 00:49:59 -0300

    Joining tabs and repos component

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Search from '../search/search';
+import Repos from '../repository/repository'
 import './tabs.css'
 
 const Tabs = ({repos, starred}) => {
@@ -7,7 +8,7 @@ const Tabs = ({repos, starred}) => {
   const component = (
     <div className='ml-5'>
       <nav>
-        <div className="nav nav-tabs mt-5" id="nav-tab" role="tablist">
+        <div className="nav nav-tabs mt-4" id="nav-tab" role="tablist">
           <a className="nav-item nav-link text-muted active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Repos</a>
           <a className="nav-item nav-link text-muted" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Starred</a>
         </div>
@@ -16,9 +17,8 @@ const Tabs = ({repos, starred}) => {
       <Search />
 
       <div className="tab-content" id="nav-tabContent">
-        <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home</div>
-        <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Profile</div>
-        <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Contact</div>
+        <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"> <Repos repos={repos} /> </div>
+        <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"> <Repos repos={starred} /> </div>
       </div>
     </div>
   );
