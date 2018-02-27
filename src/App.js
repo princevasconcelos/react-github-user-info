becbe76f6449d399c2a586f75797441dd8637661
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-02-27 12:24:30 -0300

    Change username to rodgerpaulo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      username: 'willianjusten', //'rodgerpaulo'
+      username: 'rodgerpaulo'
     }
   }
 
